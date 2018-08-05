af943a58934bb6ac9a649dc294d038fa676d03c0
andrewlor <andrewlor332@gmail.com>
2018-08-04 20:45:57 -0400

    modify serializers

--- app/serializers/api/v1/journey_serializer.rb
+++ app/serializers/api/v1/journey_serializer.rb
@@ -1,4 +1,5 @@
 class Api::V1::JourneySerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :title, :mission_statement
+  attributes :id, :user_id, :title, :mission_statement, :created_at
   has_many :journey_logs
+  belongs_to :user
 end
