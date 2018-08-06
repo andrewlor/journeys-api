e564cf78823c61e6ab9481386a0caa1324736926
andrewlor <andrewlor332@gmail.com>
2018-08-05 20:59:54 -0400

    add prefix to journey

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -2,4 +2,12 @@ class Journey < ApplicationRecord
   belongs_to :user
   has_many :journey_logs
   has_many :data_points
+
+  before_save :append_prefix
+
+  private
+
+  def append_prefix
+    self.title = "My Journey to #{self.title}"
+  end
 end
